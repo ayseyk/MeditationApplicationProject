8b49d91d672f26dc1ca7d986df9a83c3fdb2c7a1
Ayşe YALÇINKAYA <ayse_ykaya@hotmail.com>
2022-07-27 11:02:22 +0300

    Update media screen

--- app/src/main/java/com/example/meditation/view/MediaFragment.kt
+++ app/src/main/java/com/example/meditation/view/MediaFragment.kt
@@ -7,9 +7,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.example.meditation.R
+import com.example.meditation.databinding.FragmentMediaBinding
 import com.example.meditation.model.Story
 import kotlinx.android.synthetic.main.fragment_media.*
 import java.io.IOException
@@ -19,6 +21,7 @@ import java.util.*
 class MediaFragment : Fragment() {
 
     private var story : Story? = null
+    private lateinit var dataBinding : FragmentMediaBinding
     private lateinit var mediaPlayer: MediaPlayer
     private val audioUrl = "https://d2r0ihkco3hemf.cloudfront.net/bgxupraW2spUpr_y2.mp3"
 
@@ -26,7 +29,8 @@ class MediaFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_media, container, false)
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_media, container, false)
+        return dataBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -35,6 +39,8 @@ class MediaFragment : Fragment() {
         arguments?.let {
             story = MediaFragmentArgs.fromBundle(it).story
         }
+
+        dataBinding.story = story
 
         initializeViews()
         initializeEvents(view)
@@ -45,9 +51,6 @@ class MediaFragment : Fragment() {
         val calendar = Calendar.getInstance()
         val days = resources.getStringArray(R.array.days)
         tvDate.text = SimpleDateFormat("dd/MM/yyyy").format(Date()) +", "+ days[calendar.get(Calendar.DAY_OF_WEEK)-1]
-
-        tvSName.text = story?.Name
-        tvStoryLong.text = story?.LongDescription
     }
 
     private fun initializeEvents(view : View) {
